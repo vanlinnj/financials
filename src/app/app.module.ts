23672b755dc877f8aad6433a7bc747537cd333fe
vanlinnj <vanlinj@gmail.com>
2020-06-03 16:51:53 -0500

    Created shell for bills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillsComponent } from './bills/bills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillsComponent
   ],
   imports: [
     BrowserModule,
