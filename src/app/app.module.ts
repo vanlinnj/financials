693422f483d5c79aaf87b082fb1aa6f635d88dbd
vanlinnj <vanlinj@gmail.com>
2020-06-03 18:32:39 -0500

    Added shell components for account - create/recover/update. Added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BillsComponent } from './bills/bills.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { RecoverComponent } from './account/recover/recover.component';
+import { CreateComponent } from './account/create/create.component';
+import { UpdateComponent } from './account/update/update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BillsComponent
+    BillsComponent,
+    LoginComponent,
+    AccountComponent,
+    RecoverComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
